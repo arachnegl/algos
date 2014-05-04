9a0085813d2acf0fa55088b31d5375d352e96c43
arachnegl <gregoryloyse@gmail.com>
2014-05-04 14:05:08 +0100

    insertion sort - can sort ordered list of len 2

--- insertion_sort.py
+++ insertion_sort.py
@@ -6,8 +6,14 @@ chapter 2
 import unittest
 
 
-def sort(i):
-    return i
+def sort(sequence):
+    """ Sorts sequence using insertion sort
+    """
+    # to retain polymorphism we order in place,
+    # we assume sequence supports indexing
+    if len(sequence) > 1 and sequence[0] > sequence[1]:
+        sequence[0], sequence[1] = sequence[1], sequence[0]
+    return sequence
 
 
 class TestInsertionSort(unittest.TestCase):
@@ -33,6 +39,15 @@ class TestInsertionSort(unittest.TestCase):
     def test_can_sort_ordered_list_of_length_two(self):
 
         input_ = [1, 2]
+
+        output = sort(input_)
+        expected = [1, 2]
+
+        self.assertSequenceEqual(output, expected)
+
+    def test_can_sort_unordered_list_of_length_two(self):
+
+        input_ = [2, 1]
 
         output = sort(input_)
         expected = [1, 2]
