f53d339365cfa64538ada69b4fa714927d21de9c
greg loyse <gloyse@google.com>
2015-01-02 19:08:10 +0000

    stack implementation

--- stack.py
+++ stack.py
@@ -1,4 +1,31 @@
+"""
+Stack implementation and related uses.
+
+Run tests:
+    python3 -m unittest stack.py
+"""
 import unittest
+
+
+class Stack:
+
+    def __init__(self):
+        self.items = list()
+
+    def is_empty(self):
+        return bool(self.items)
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
 
 
 def is_balanced(string, openclose='()'):
